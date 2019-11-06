93162e42ee89d82425bcaf987a4ff298568c656b
tomasMizera <tomas.mizera2@gmail.com>
2019-11-06 20:40:04 +0100

    database setup

---
+++ coastr/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import os
+
+
+def create_app(_test_config=None):
+    
+    #create application
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
+
+    if _test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(_test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    @app.route('/hello')
+    def hello():
+        return "Hi, structured project!"
+
+    return app
+
---
+++ coastr/db.py
@@ -0,0 +1,38 @@
+# Here DB connection will be established
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = None
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('db.py') as f:
+        pass
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
